4601a5142cb0298ffe953e1e7b4cfa4542a01c91
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-01-22 15:22:51 +0530

    Add files via upload

---
+++ name.c
@@ -0,0 +1,9 @@
+// A C program to print your name
+#include <stdio.h>
+int main(){
+  char name[50];
+  printf("\nEnter your name : ");
+  scanf("%s",name);
+  printf("Welcome %s",name);
+  return 0;
+}
