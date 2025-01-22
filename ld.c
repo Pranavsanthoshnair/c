470629154731069a34833dd68ba52e300da07256
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-01-22 16:23:36 +0530

    Add files via upload

---
+++ ld.c
@@ -0,0 +1,11 @@
+// A C program to find The byte size of some data types
+#include <stdio.h>
+int main(){
+  printf("\nSize of integer=%ld",sizeof(int));
+  printf("\nSize of float=%ld",sizeof(float));
+  printf("\nSize of double=%ld",sizeof(double));
+  printf("\nSize of char=%ld",sizeof(char));
+  printf("\nSize of long int=%ld",sizeof(long int));
+  printf("\nSize of short int=%ld",sizeof(short int));
+  return 0;
+}
