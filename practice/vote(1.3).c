bfc9417a5b4825008b9cb20af0b33e638138293f
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-01-28 20:30:27 +0530

    Add files via upload

---
+++ practice/vote(1.3).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age >= 18) {
+        printf("Congratulation! You are eligible for casting your vote.\n");
+    } else {
+        printf("Sorry, you are not eligible to vote.\n");
+    }
+    return 0;
+}
