fcb821ef97cefc26310234da3735bc64a3ebe865
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-01-29 15:30:06 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,24 @@
+/*
+Pranav S Nair
+Date - 29/1/25
+Purpose - to find factorial of a number 
+*/
+#include<stdio.h>
+int main(){
+  int num,orgnum,fact = 1;
+  printf("Enter Number : ");
+  scanf("%d",&num);
+  orgnum = num;
+  if(num>0){
+    while(num>1){
+      fact *= num;
+      num -= 1;
+    }
+  printf("%d is the factorial of %d",fact,orgnum);
+  }   
+  else{
+  printf("The number is negative");
+  }
+  
+  return 0;
+}
