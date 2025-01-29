7e26e601c53f87129ca329312ad88d7e21feb1a9
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-01-29 14:54:58 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,41 @@
+/*
+Pranav S Nair
+Date - 29/1/25
+Purpose - A Simple Calculator
+*/
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+    char operator;
+
+    printf("Enter first integer: ");
+    scanf("%d", &num1);
+    printf("Enter second integer: ");
+    scanf("%d", &num2);
+    printf("Enter operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
