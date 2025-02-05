8dbfd3c700a53ab99e3d6331ac21e86003698113
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-02-05 14:08:37 +0530

    Add files via upload

---
+++ seq.c
@@ -0,0 +1,39 @@
+/*
+Author - Pranav S Nair
+Date - 5/2/25
+Purpose - TO search for an element in an array
+*/
+#include <stdio.h>
+
+int main() {
+    int num, i, limit, position = -1;
+    
+    printf("Enter number of elements in an array: ");
+    scanf("%d", &limit);
+    
+    int arrnum[limit];        // declaring array
+    
+    printf("Enter the %d numbers: ", limit);
+    for (i = 0; i < limit; i++) { 
+        scanf("%d", &arrnum[i]);
+    }
+    
+    printf("Enter element to search: ");
+    scanf("%d", &num);
+    
+    for (i = 0; i < limit; i++) {
+        if (arrnum[i] == num) {
+            position = i;  
+            break; 
+        }
+    }
+    
+    if (position != -1) {
+        printf("Element found at position: %d\n", position+1);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
+
