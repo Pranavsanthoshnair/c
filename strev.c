6b557fc53ed49b084743f3a1b6850fcb15ce784f
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-02-19 14:10:58 +0530

    Update strev.c

--- strev.c
+++ strev.c
@@ -1,3 +1,8 @@
+/*
+Author - Pranav S Nair
+Date - 19/02/2025
+Purpose - To print reverse of a string without inbuilt functions.
+*/
 #include<stdio.h>
 int main(){
     char str[50],temp;
@@ -6,7 +11,7 @@ int main(){
     int i,l,j;
     for(i=0;str[i]!='\0';i++);
     l=i-1;
-    for(j=0;j<=l/2;j++){
+    for(j=0;j<=l;j++){
         temp=str[l];
         str[l]=str[j];
         str[j]=temp;
@@ -16,4 +21,3 @@ int main(){
     puts(str);
     return 0;
 }
-
